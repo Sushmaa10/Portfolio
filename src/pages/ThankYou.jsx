b0b8ec04e2c0675c761547fd6993e81e209c5dff
Sushmaa10 <sushmasush2210@gmail.com>
2025-07-15 03:37:04 +0530

    Add dummy form to public for Netlify detection

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ThankYou = () => {
   return (
     <div style={{ padding: "80px", textAlign: "center" }}>
-      <h2>Thank you! 🎉</h2>
+      <h2>Thank you for contacting me!</h2>
       <p>Your message has been sent successfully.</p>
     </div>
   );
